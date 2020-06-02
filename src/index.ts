90c40ecd6d292247ff96fb384b30b3373cb0207b
Vilberg <vilbergs@gmail.com>
2020-06-02 14:23:45 +0200

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { serve } from 'https://deno.land/std/http/server.ts'
+import { readJson } from 'https://deno.land/std/fs/read_json.ts'
+
+const server = serve({ port: 8000 })
+
+for await (const req of server) {
+  const haiku = await randomHaiku()
+
+  req.respond({ body: haiku })
+}
+
+async function randomHaiku() {
+  return randomArrayItem(await readHaikus())
+}
+
+async function readHaikus(): Promise<string[]> {
+  const haikus = await readJson('./haikus.json')
+
+  if (!validateHaikus(haikus)) {
+    throw Error('Error reading from Haiku repository, check that the source file is valid')
+  }
+
+  return haikus
+}
+
+function validateHaikus(array: unknown): array is string[] {
+  return array instanceof Array && array.every((value) => typeof value === 'string')
+}
+
+function randomArrayItem(items: string[]): string {
+  const index = Math.floor(Math.random() * items.length)
+
+  return items[index]
+}
